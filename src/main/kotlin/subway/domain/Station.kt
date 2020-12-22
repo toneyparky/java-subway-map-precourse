d46ea60fdb03806a7bc2a356eeb1b81553ab8ff8
toneyparky <qkrtmddhks95@gmail.com>
2020-12-22 21:16:31 +0900

    chore(station): equals, hashcode 추가

--- src/main/kotlin/subway/domain/Station.kt
+++ src/main/kotlin/subway/domain/Station.kt
@@ -8,4 +8,19 @@ class Station(private val name: String) {
     fun isSame(name: String): Boolean {
         return this.name == name
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Station
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
 }
